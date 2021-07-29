bcc9acea923a95f7a53457fca375664ab992dd3c
lizhijie429 <632163606@qq.com>
2021-07-29 14:52:03 +0800

    升级qiankun-vue2-common包

--- sub02/src/main.js
+++ sub02/src/main.js
@@ -4,7 +4,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import common from "qiankun-vue2-common";
-import "qiankun-vue2-common/iconfont/iconfont.css";
 Vue.config.productionTip = false;
 
 import ElementUI from "element-ui";
