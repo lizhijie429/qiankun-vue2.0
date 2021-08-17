7d2176e7202e96058fa16499a5cdba79f0dc12b3
lizhijie429 <632163606@qq.com>
2021-08-17 14:55:03 +0800

    部署编译测试

--- main/src/qiankun/index.js
+++ main/src/qiankun/index.js
@@ -2,30 +2,19 @@
  * @Author: lizhijie429
  * @Date: 2021-08-16 17:11:52
  * @LastEditors: lizhijie429
- * @LastEditTime: 2021-08-16 17:28:20
+ * @LastEditTime: 2021-08-17 14:25:16
  * @Description:
  */
 import { registerMicroApps, start } from "qiankun";
-import { initialState } from "./globalState";
-import { microApps, config } from "./registerApp";
+
+import { filterApps, config } from "./registerApp";
 
 export { qiankunActions } from "./globalState";
 
-/* 封装全局下发的数据及挂载子应用的节点 */
-const apps = microApps.map((item) => {
-  return {
-    ...item,
-    container: "#subapp-viewport", // 子应用挂载的div
-    props: {
-      routerBase: item.activeRule, // 下发基础路由
-      globalState: initialState, // 下发全局数据方法
-    },
-  };
-});
-
 // 微应用注册
 export const registerApps = () => {
-  registerMicroApps(apps, config);
+  const _apps = filterApps();
+  registerMicroApps(_apps, config);
   start({
     prefetch: "all", // 可选，是否开启预加载，默认为 true。
     sandbox: true, // 可选，是否开启沙箱，默认为 true。// 从而确保微应用的样式不会对全局造成影响。
--- main/src/qiankun/registerApp.js
+++ main/src/qiankun/registerApp.js
@@ -1,16 +1,38 @@
+import { initialState } from "./globalState";
 /* 子应用列表 */
-export const microApps = [
+const microApps = [
   {
-    name: "sub01", // 应用名称
+    // 应用名称
+    name: "sub01",
+    // 必选，微应用的入口。
     entry: process.env.VUE_APP_CHILD_sub01,
-    activeRule: "/sub01",
   },
   {
     name: "sub02",
     entry: process.env.VUE_APP_CHILD_sub02,
-    activeRule: "/sub02",
   },
 ];
+
+/* 路由监听 */
+const genActiveRule = (routerPrefix) => {
+  return (location) => location.pathname.startsWith(routerPrefix);
+};
+
+/* 重构apps */
+export const filterApps = () => {
+  microApps.forEach((item) => {
+    // 必选，微应用的容器节点的选择器或者 Element 实例。
+    item.container = "#subapp-viewport";
+    // 必选，微应用的激活规则。
+    item.activeRule = genActiveRule(`/${item.name}`);
+    // 可选，主应用需要传递给微应用的数据。
+    item.props = {
+      routerBase: item.activeRule, // 下发基础路由
+      globalState: initialState, // 下发全局数据方法
+    };
+  });
+  return microApps;
+};
 
 /* qiankun全局声明周期钩子 */
 export const config = {
