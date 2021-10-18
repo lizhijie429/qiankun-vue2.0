a861844b9f57a8b12940142dc6c5c2db5fcdd356
lizhijie429 <632163606@qq.com>
2021-10-18 17:22:40 +0800

    升级qiankun依赖

--- main/src/qiankun/index.js
+++ main/src/qiankun/index.js
@@ -2,7 +2,7 @@
  * @Author: lizhijie429
  * @Date: 2021-08-16 17:11:52
  * @LastEditors: lizhijie429
- * @LastEditTime: 2021-08-20 09:10:01
+ * @LastEditTime: 2021-10-18 17:14:01
  * @Description:
  */
 import { registerMicroApps, start } from "qiankun";
@@ -17,7 +17,7 @@ export const registerApps = () => {
   registerMicroApps(_apps, microConfig);
   start({
     prefetch: "all", // 可选，是否开启预加载，默认为 true。
-    sandbox: true, // 可选，是否开启沙箱，默认为 true。// 从而确保微应用的样式不会对全局造成影响。
+    sandbox: { strictStyleIsolation: true }, // 可选，是否开启沙箱，默认为 true。// 从而确保微应用的样式不会对全局造成影响。
     singular: true, // 可选，是否为单实例场景，单实例指的是同一时间只会渲染一个微应用。默认为 true。
   });
 };
--- main/src/qiankun/registerApp.js
+++ main/src/qiankun/registerApp.js
@@ -43,15 +43,25 @@ export const microConfig = {
     (app) => {
       console.log("%c before load", "background:#3a5ab0 ; padding: 1px; border-radius: 3px;  color: #fff", app);
     },
-  ], // 挂载前回调
+  ], // 预加载
   beforeMount: [
     (app) => {
       console.log("%c before mount", "background:#7d9553 ; padding: 1px; border-radius: 3px;  color: #fff", app);
     },
-  ], // 挂载后回调
+  ], // 挂载前回调
+  // afterMount: [
+  //   (app) => {
+  //     console.log("%c after mount", "background:#7d7453 ; padding: 1px; border-radius: 3px;  color: #fff", app);
+  //   },
+  // ], // 挂载后回调
+  // beforeUnmount: [
+  //   (app) => {
+  //     console.log("%c before unmount", "background:#7dd253 ; padding: 1px; border-radius: 3px;  color: #fff", app);
+  //   },
+  // ], // 卸载前回调
   afterUnmount: [
     (app) => {
-      console.log("%c after unload", "background:#d2525c ; padding: 1px; border-radius: 3px;  color: #fff", app);
+      console.log("%c after unmount", "background:#d2525c ; padding: 1px; border-radius: 3px;  color: #fff", app);
     },
   ], // 卸载后回调
 };
